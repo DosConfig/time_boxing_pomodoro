92bdc2dff89b80a92c5af478b3eebddd2ee63812
John Doe <qlqjsdmsz8@gmail.com>
2026-07-21 20:06:29 +0900

    chore(repo): remove generated iOS build artifacts

--- ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/GeneratedPluginRegistrant.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/seongwoo/Documents/GitHub/pomodoro_method_channel/ios/Runner/GeneratedPluginRegistrant.m \
-  /Users/seongwoo/Documents/GitHub/pomodoro_method_channel/ios/Runner/GeneratedPluginRegistrant.h \
-  /Users/seongwoo/Documents/GitHub/pomodoro_method_channel/ios/build/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap
--- ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/seongwoo/Documents/GitHub/pomodoro_method_channel/ios/build/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
